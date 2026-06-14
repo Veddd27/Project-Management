b34dcc8cd516eb54241eb59bed01daa5cca6bc46
Ved Ambulkar <ved2702ambulkar@gmail.com>
2026-06-14 13:09:01 +0530

    Added controllers multer middleware and routes

--- src/app.js
+++ src/app.js
@@ -4,25 +4,34 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+// basic configurations
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
 
+// cors configurations
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://locahost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
-import healthCheckRouter from "./routes/healthcheck.routes.js";
-app.use("/api/v1/healthcheck", healthCheckRouter);
+//  import the routes
 
+import healthCheckRouter from "./routes/healthcheck.routes.js";
 import authRouter from "./routes/auth.routes.js";
-app.use("/api/v1/auth", authRouter)
+import projectRouter from "./routes/project.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/projects", projectRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to basecampy");
+});
 
 export default app;
-
